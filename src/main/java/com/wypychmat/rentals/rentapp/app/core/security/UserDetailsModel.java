a6f5f3ad6c0768505750140f773a0146c878e45e
Mateusz Wypych <wypychmat@gmail.com>
2021-03-07 17:01:18 +0100

    added security models and services

---
+++ src/main/java/com/wypychmat/rentals/rentapp/app/core/security/UserDetailsModel.java
@@ -0,0 +1,57 @@
+package com.wypychmat.rentals.rentapp.app.core.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+class UserDetailsModel implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final Set<? extends GrantedAuthority> grantedAuthorities;
+    private final boolean isEnabled;
+
+    UserDetailsModel(String username, String password, Set<? extends GrantedAuthority> grantedAuthorities, boolean isEnabled) {
+        this.username = username;
+        this.password = password;
+        this.grantedAuthorities = grantedAuthorities;
+        this.isEnabled = isEnabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
