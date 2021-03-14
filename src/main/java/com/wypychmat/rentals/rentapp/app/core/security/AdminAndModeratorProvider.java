6d6cc665dd4eb798ff84b04954c823998d8085c5
Mateusz Wypych <wypychmat@gmail.com>
2021-03-14 12:50:02 +0100

    change to create moderator or Admin on startup

---
+++ src/main/java/com/wypychmat/rentals/rentapp/app/core/security/AdminAndModeratorProvider.java
@@ -0,0 +1,41 @@
+package com.wypychmat.rentals.rentapp.app.core.security;
+
+import com.wypychmat.rentals.rentapp.app.core.user.User;
+import com.wypychmat.rentals.rentapp.app.core.user.constant.ApplicationMainRole;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.PostConstruct;
+
+abstract class AdminAndModeratorProvider {
+    private static final String EMAIL_SUFFIX = "@admin.com";
+    public static final String ADMIN = "admin";
+    private final CreateAdminAndModeratorService createAdminAndModeratorService;
+    private final PasswordEncoder passwordEncoder;
+
+    protected AdminAndModeratorProvider(CreateAdminAndModeratorService createAdminAndModeratorService, PasswordEncoder passwordEncoder) {
+        this.createAdminAndModeratorService = createAdminAndModeratorService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    protected abstract AdminOrModerator prepareAdminCredentials();
+    protected abstract AdminOrModerator prepareModeratorCredentials();
+
+
+    @PostConstruct
+    void createManagementUser() {
+        createSuperUserIfNotExist(prepareAdminCredentials(),ApplicationMainRole.ADMIN);
+        createSuperUserIfNotExist(prepareModeratorCredentials(),ApplicationMainRole.MODERATOR);
+    }
+
+    private void createSuperUserIfNotExist(AdminOrModerator adminOrModerator, ApplicationMainRole appRole) {
+        String name = adminOrModerator.getName();
+        User user = new User();
+        user.setUsername(name);
+        user.setPassword(passwordEncoder.encode(adminOrModerator.getPassword()));
+        user.setEmail(name + EMAIL_SUFFIX);
+        user.setFirstName(name);
+        user.setLastName(ADMIN);
+        user.setEnabled(true);
+        createAdminAndModeratorService.saveAdminOrModIfNotExist(user,appRole);
+    }
+}
--- src/main/java/com/wypychmat/rentals/rentapp/app/core/security/AdminOrModerator.java
+++ src/main/java/com/wypychmat/rentals/rentapp/app/core/security/AdminOrModerator.java
@@ -1,16 +1,16 @@
 package com.wypychmat.rentals.rentapp.app.core.security;
 
-class Admin {
-    private final String adminName;
+class AdminOrModerator {
+    private final String name;
     private final String password;
 
-    Admin(String adminName, String password) {
-        this.adminName = adminName;
+    AdminOrModerator(String getName, String password) {
+        this.name = getName;
         this.password = password;
     }
 
-    String getAdminName() {
-        return adminName;
+    String getName() {
+        return name;
     }
 
     String getPassword() {
